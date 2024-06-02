dc4a5e72168fc84a3616d07c7946c75e016ce274
EpicOfficer <9379778+EpicOfficer@users.noreply.github.com>
2024-06-02 22:20:04 +0100

    Don't need a UI to be honest

--- main.js
+++ main.js
@@ -1,61 +1,58 @@
 const path = require('path');
 const {app, ipcMain, Tray, Menu, BrowserWindow} = require ('electron');
 
+const frequency = 10; // 10hz tone
+const interval = 1500; // 25 minutes
+
 let win;
 let tray;
 
+function createTray() {
+    tray = new Tray(path.join(__dirname, 'tray.png'));
+
+    const contextMenu = Menu.buildFromTemplate([
+        {label: 'Test', click: () => win.webContents.executeJavaScript(`playTone(${frequency});`)},
+        {label: 'Exit', click:  function(){ app.quit(); } }
+    ]);
+
+    tray.setContextMenu(contextMenu);
+}
+
 function createWindow () {
     win = new BrowserWindow({
-        width: 400,
-        height: 300,
+        width: 225,
+        height: 175,
         show: false,
         resizable: false,
         autoHideMenuBar: true,
         darkTheme: true,
         webPreferences: {
             nodeIntegration: true,
+            enableRemoteModule: true,
             backgroundThrottling: false
         }
     });
 
-    win.on('close', function (event) {
-        if (app.quitting) {
-            win = null
-        } else {
-            event.preventDefault()
-            win.hide()
-        }
-    })
-
-    win.loadFile(path.join(__dirname, 'index.html')).then();
-
-    win.webContents.once('dom-ready', () => {
-        win.webContents.executeJavaScript('playTone(); setToneInterval();');
-    });
+    win.loadURL(`data:text/html,
+        <script>
+            const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+        
+            function playTone(freq) {
+                const oscillator = audioCtx.createOscillator();
+                oscillator.type = 'sine';
+                oscillator.frequency.value = freq;
+                oscillator.connect(audioCtx.destination);
+                oscillator.start();
+                setTimeout(() => oscillator.stop(), 1000);
+            }
+        </script>
+  `).then();
 }
 
 app.whenReady().then(() => {
-    tray = new Tray(path.join(__dirname, 'tray.png'));
-
-    const contextMenu = Menu.buildFromTemplate([
-        {label: 'Open', click: function() {win.show();}},
-        {label: 'Exit', click: function() {
-            app.quitting = true;
-            app.quit();
-        }}
-    ]);
-    
-    tray.setContextMenu(contextMenu);
-    
+    createTray();
     createWindow();
 
-    app.on('activate', () => {
-        win.show();
-    });
+    win.webContents.executeJavaScript(`playTone(${frequency});`);
+    setInterval(() => { win.webContents.executeJavaScript(`playTone(${frequency});`); }, interval * 1000);
 })
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
